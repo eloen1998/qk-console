a1d56f94370efc88273894fa42de95fbbf15b7a9
eloen <597962307@qq.com>
2023-05-24 21:17:28 +0800

    feat: 优化test中读文件方式，图标引入修改

--- src/test/vue-delete.spec.ts
+++ src/test/vue-delete.spec.ts
@@ -1,17 +1,17 @@
 import { describe, it, expect } from "vitest";
 import { getConsoleRangeVue } from "../handler/handleVue";
-import readFile from "../utils/readFile";
 import path from "path";
+import fs from "fs";
 
-describe("get consoleRange in vue", async () => {
-    const code = await readFile(path.join(__dirname, "./code.vue"));
+describe("get consoleRange in vue", () => {
+    const code = fs.readFileSync(path.join(__dirname, "./code.vue"), "utf-8");
     it("this变量识别", () => {
         const consoleRange = getConsoleRangeVue(code);
         expect(consoleRange).toEqual([
             {
                 name: "log",
-                range: [446, 465],
-            },
+                range: [446, 465]
+            }
         ]);
     });
 });
--- src/test/vue.spec.ts
+++ src/test/vue.spec.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { getVariableVue } from "../handler/handleVue";
-import readFile from "../utils/readFile";
 import path from "path";
+import fs from "fs";
 
 describe("get variable in vue", async () => {
-    const code = await readFile(path.join(__dirname, "./code.vue"));
+    const code = fs.readFileSync(path.join(__dirname, "./code.vue"), "utf-8");
     it("this变量识别", () => {
         const name = getVariableVue(code, 387);
         expect(name).toEqual({ variables: ["this.bb.ccc"] });
